6aacaced8930ae0f6315811f6449012cfecd0ee1
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-01-30 22:40:56 -0500

    Added two more interfaces.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -17,3 +17,21 @@ interface Project {
   codeUrl: string;
   demoUrl: string;
 }
+
+interface ContactFormData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+interface EmailData {
+  service_id: string;
+  template_id: string;
+  user_id: string;
+  template_params: {
+    from_name: string;
+    from_email: string;
+    to_name: string;
+    message: string;
+  }
+}
